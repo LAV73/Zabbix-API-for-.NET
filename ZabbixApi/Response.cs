a377671fd55c4be6f4ecdd44a84aea122542988f
Yosuke Oyama <yoyama@alpari.jp>
2015-01-26 04:29:31 +0000

    first commit

---
+++ ZabbixApi/Response.cs
@@ -0,0 +1,10 @@
+﻿namespace Ysq.Zabbix
+{
+    public class Response
+    {
+        public int Id { get; set; }
+        public string JsonRpc { get; set; }
+        public dynamic Result { get; set; }
+        public ZabbixError Error { get; set; }
+    }
+}
